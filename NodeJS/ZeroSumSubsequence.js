65dd451e3d2b9afcaf5a346bb78289ffbb88e9a9
patrik_masiar <masiar.patrik@gmail.com>
2020-09-11 20:59:42 +0200

    Update ZeroSumSubsequence.js

--- NodeJS/ZeroSumSubsequence.js
+++ NodeJS/ZeroSumSubsequence.js
@@ -3,8 +3,8 @@ function isArrayWithZeroSumSubsequence(array => {
   set.add(0);
   let sum = 0;
   
-  for(let i = 0; i < inputArray.length; i++){
-    sum = sum + inputArray[i];
+  for(let i = 0; i < array.length; i++){
+    sum = sum + array[i];
     if(set.has(sum)){
       return true;
     }
